573b82f89b6c4cb04bf01c9651839299e00cedd2
John Cartpenter <johncarpenter2008@gmail,com>
2025-03-03 10:30:38 -0600

    created constructor

--- hotel.py
+++ hotel.py
@@ -1,3 +1,9 @@
 class Hotel:
-	pass
-		
+    def __init__(self, name, address, floorCount, floorRatio):
+        self.name = name
+        self.address = address
+        self.rooms = []
+        for floor in range(floorCount):
+            self.rooms.append([])
+            for room in range(floorRatio):
+                self.rooms[floor].append("")
--- hotel_test.py
+++ hotel_test.py
@@ -1,1 +1,4 @@
 from hotel import Hotel
+
+hotel = Hotel("Holiday Inn","16 Wolf Road", 3, 20)
+print(hotel.name)
