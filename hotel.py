7163fb29cc6044a768bdebb245eac8ecd9331341
John Cartpenter <johncarpenter2008@gmail,com>
2025-03-05 10:56:39 -0600

    Added canceleing and error checking

--- hotel.py
+++ hotel.py
@@ -9,9 +9,42 @@ class Hotel:
                 self.rooms[floor].append("")
 
     def BookReservation(self, name,roomNumber):
+        floor, roomIndex = self.convertNumberToRoom(roomNumber)
+        if not self.validateRoomNumber(floor,roomIndex):
+            print("Invalid room number")
+            return
+        self.rooms[floor][roomIndex] = name
+
+    def CancelReservation(self,roomNumber):
+        floor, roomIndex = self.convertNumberToRoom(roomNumber)
+        if not self.validateRoomNumber(floor,roomIndex):
+            print("Invalid room number")
+            return
+        name = self.rooms[floor][roomIndex]
+        self.rooms[floor][roomIndex] = ""
+        print("Reservation for " + name + " under room " + str(roomNumber) + " has been canceled")
+
+    def SeeReservation(self, roomNumber):
+        floor, roomIndex = self.convertNumberToRoom(roomNumber)
+        if not self.validateRoomNumber(floor,roomIndex):
+            print("Invalid room number")
+            return
+        if self.rooms[floor][roomIndex] != "":
+            print(self.rooms[floor][roomIndex] + " Is staying in room " + str(roomNumber) + "." )
+        else:
+            print("Room " + str(roomNumber) + " is vacant.")
+
+    def convertNumberToRoom(self, roomNumber):
         roomstr = str(roomNumber)
         floor = int(roomstr[0])
         roomIndex = int(roomstr[1:3])
-        self.rooms[floor][roomIndex] = name
+        return floor, roomIndex
+
+    def validateRoomNumber(self,floor,roomIndex):
+        if floor >= 0 and floor < len(self.rooms) and roomIndex >= 0 and roomIndex < len(self.rooms[floor]):
+            return True
+        else:
+            return False
+
 
 
--- hotel_test.py
+++ hotel_test.py
@@ -3,5 +3,11 @@ from hotel import Hotel
 hotel = Hotel("Holiday Inn","16 Wolf Road", 3, 20)
 print(hotel.name)
 hotel.BookReservation("John", 205)
-print(hotel.rooms[2][5])
-print(hotel.rooms)
+hotel.SeeReservation(205)
+hotel.SeeReservation(115)
+hotel.CancelReservation(205)
+hotel.SeeReservation(205)
+hotel.BookReservation("John", 305)
+hotel.CancelReservation(265)
+hotel.CancelReservation(201)
+
